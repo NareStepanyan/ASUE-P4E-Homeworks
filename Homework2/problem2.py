095a4f09feded06439ceab9a0ce9ae36494e7e96
NareStepanyan <nare.stepanyan97@gmail.com>
2018-11-09 00:43:32 +0400

    add problem2.py

--- Homework2/problem2.py
+++ Homework2/problem2.py
@@ -1,18 +1,23 @@
 import random
 num = random.randint(1,100)
-guess=int(input("guess number: "))
+print(num)
+guess=input("guess number: ")
 i=0
-while num != guess:
-	if i>3:
-		print("game over!")
-		break			
-	if num > guess:
-		print("too high!")
-	else:
-		print("too low!")
-	i+=1
-	guess = int(input("guess number: "))
-		
-			
+if (guess) == "exit":
+	print("exit !")	
 else:
-	print("exactly right!")	
+	while num != int(guess):	
+		if i>3:
+			print("game over!")
+			break			
+		if num > int(guess):
+			print("too high!")
+		else:
+			print("too low!")
+		i+=1
+		guess = input("guess number: ")	
+		if (guess) == "exit":
+			print("exit !")	
+			break			
+	else:
+		print("exactly right!")	
